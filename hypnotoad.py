e14c95bc04d2dbc6290ee5e35943faf97a7f591d
remoheadder <47491287+remoheadder@users.noreply.github.com>
2021-09-01 11:56:04 -0600

    Update hypnotoad.py

--- hypnotoad.py
+++ hypnotoad.py
@@ -1,3 +1,4 @@
 import os
 
+os.system("clear > /dev/tty1") #clear any text on the Pi's screen so it is blank between episodes
 os.system("omxplayer --orientation 180 -o local /home/pi/futurama/videos/FuturamaS00e04.m4v") #Play the Hypnotoad episode, flip screen 180 degrees and output to headphone jack
