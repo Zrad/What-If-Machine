4ea43b16bed1c1d37b491e68638235d6a39a062d
remoheadder <47491287+remoheadder@users.noreply.github.com>
2021-09-01 08:51:44 -0600

    Create hypnotoad.py

---
+++ hypnotoad.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("omxplayer --orientation 180 -o local /home/pi/futurama/videos/FuturamaS00e04.m4v") #Play the Hypnotoad episode, flip screen 180 degrees and output to headphone jack
