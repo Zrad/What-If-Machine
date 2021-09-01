b43a488eedfa817616a832644ef89e94673a2cb6
remoheadder <47491287+remoheadder@users.noreply.github.com>
2021-09-01 11:56:20 -0600

    Update player.py

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
 import os
 
+os.system("clear > /dev/tty1") #clear any text on the Pi's screen so it is blank between episodes
 os.system("while true; do omxplayer --orientation 180 -o local $(ls /home/pi/futurama/videos/* | shuf | head -n1); done") #indefinitely play Futurama episodes on Omxplayer, flipped 180 degrees and outputting on headphone jack)
